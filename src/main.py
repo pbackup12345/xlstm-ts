bd03106dc9a38d81a6a98a8bac748151918d5509
Gonzalo López Gil <gonzalo.lopezg@edu.uah.es>
2024-07-27 18:49:07 +0200

    fix: API key removed

--- src/main.py
+++ src/main.py
@@ -5,7 +5,6 @@ from gui.app import StockDataViewer
 import tkinter as tk
 
 if __name__ == "__main__":
-    os.environ['TIINGO_API_KEY'] = 'e24d4765870dd6588f8c4706b4301dd7001aa4ba'
     root = tk.Tk()
     app = StockDataViewer(root)
     root.mainloop()
