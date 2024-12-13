fa7de04457f36586217be0d7458e557b16f1f869
pbackup12345 <47710583+pbackup12345@users.noreply.github.com>
2024-12-13 14:08:19 +0100

    Update constants.py

--- src/ml/constants.py
+++ src/ml/constants.py
@@ -5,6 +5,6 @@
 # -------------------------------------------------------------------------------------------
 
 SEQ_LENGTH = 100
-SEQ_LENGTH_XLSTM = 100
+SEQ_LENGTH_XLSTM = 150
 RANDOM_STATE = 0
 FULL_TRAINING = False
